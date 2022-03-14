002dd781d64e2ce6ed3c3b9c1bd28e07bcf85443
Harsh <raj.11.harsh95@gmail.com>
2022-03-14 14:14:06 +0530

    Committing the bat file on github

--- artifactoryUpload.bat
+++ artifactoryUpload.bat
@@ -1,2 +1,2 @@
 cd demoApp
-curl -H "X-JFrog-Art-Api:AKCp8krASko1m7zaoCBUtgYyoRY4ZkL2fxKvarRGSdHuaVsyLdjtJADE9NTmoH8rZK7sTrVZZ" -X PUT "https://harshrajmtechproject.jfrog.io/artifactory/test-local/demoApp-0.0.1-SNAPSHOT.war" -T demoApp-0.0.1-SNAPSHOT.war
+curl -H "X-JFrog-Art-Api:AKCp8krASko1m7zaoCBUtgYyoRY4ZkL2fxKvarRGSdHuaVsyLdjtJADE9NTmoH8rZK7sTrVZZ" -X PUT "https://harshrajmtechproject.jfrog.io/artifactory/test-local/demoApp-0.0.1-SNAPSHOT.war" -T target\demoApp-0.0.1-SNAPSHOT.war
