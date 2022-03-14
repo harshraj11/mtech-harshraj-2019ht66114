c49e4fc7beed7185948f1c8a48e775103a8e8770
Harsh <raj.11.harsh95@gmail.com>
2022-03-14 07:29:11 +0530

    Committing the bat file on github

--- dockerTrigger.bat
+++ dockerTrigger.bat
@@ -1,1 +1,1 @@
-curl -X POST http://localhost:9090/job/MTech-Project-HarshRaj/job/dockerRun/build?TOKEN=118390f1bcc617377831af3585f200b38e
+curl -I http://harshraj:harshraj@localhost:9090/job/MTech-Project-HarshRaj/job/dockerRun/build?token=abcd-12345
